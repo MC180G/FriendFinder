223336d0d207894c53c74eb229cd19e16923deeb
Matt Colson <mc180g@gmail.com>
2020-01-23 10:54:03 -0500

    Finished all the guts

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,52 @@
-* A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-* A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+* A GET route with the url`/api/friends`.This will be used to display a JSON of all possible friends.
+* A POST routes`/api/friends`.This will be used to handle incoming survey results.This route will also be used to handle the compatibility logic.
+
+    console.log('API Route Connected Successfully');
+
+var friendsData = require('../data/friends.js');
+
+function apiRoutes(app) {
+    app.get('/api/friends', function (req, res) {
+        res.json(friendsData);
+    });
+
+    app.post('/api/friends', function (req, res) {
+        var newFriend = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: []
+        };
+        var scoresArray = [];
+        for (var i = 0; i < req.body.scores.length; i++) {
+            scoresArray.push(parseInt(req.body.scores[i]))
+        }
+        newFriend.scores = scoresArray;
+
+        var scoreComparisionArray = [];
+        for (var i = 0; i < friendsData.length; i++) {
+
+            var currentComparison = 0;
+            for (var j = 0; j < newFriend.scores.length; j++) {
+                currentComparison += Math.abs(newFriend.scores[j] - friendsData[i].scores[j]);
+            }
+            scoreComparisionArray.push(currentComparison);
+        }
+
+        var bestMatchPosition = 0;
+        for (var i = 1; i < scoreComparisionArray.length; i++) {
+            if (scoreComparisionArray[i] <= scoreComparisionArray[bestMatchPosition]) {
+                bestMatchPosition = i;
+            }
+
+        }
+        var bestFriendMatch = friendsData[bestMatchPosition];
+        
+        res.json(bestFriendMatch);
+
+        friendsData.push(newFriend);
+
+    });
+
+}
+
+module.exports = apiRoutes;
